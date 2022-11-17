4c4c2222f694a68d9d9ae920e854d5ee4c7123e8
mnsn123 <114860079+mnsn123@users.noreply.github.com>
2022-11-17 16:39:45 +0530

    Create notification.js

---
+++ js/notification.js
@@ -0,0 +1,35 @@
+// Initialize Firebase - https://firebase.google.com/docs/web/setup
+  var config = {
+    apiKey: "AIzaSyC2ZB7VvoUiRdUvnOSPfzP6DPqxejMiw4c",
+  authDomain: "news-pushalert.firebaseapp.com",
+  databaseURL: "https://news-pushalert.firebaseio.com",
+  projectId: "news-pushalert",
+  storageBucket: "news-pushalert.appspot.com",
+  messagingSenderId: "348845039541",
+  appId: "1:348845039541:web:0005a6bc8c69c0fc4e03fc"
+  };
+  firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+    .then(function() {
+        return messaging.getToken();
+    })
+    .then(function(token) {
+        // send rest call to add to database
+        $.ajax('https://example.firebaseio.com/pushtokens/'+token+'.json', {
+            method: 'PUT',
+            data: 'true',
+            error: function(err) {
+            }
+        });
+    })
+    .catch(function(err) {
+        console.log('Permission denied');
+    });
+
+
+
+
+
