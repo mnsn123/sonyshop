151fc21e73abd451f3d84afac64fb46c0c986682
mnsn123 <114860079+mnsn123@users.noreply.github.com>
2022-11-20 23:05:51 +0530

    Update sw-reg.js

--- js/sw-reg.js
+++ js/sw-reg.js
@@ -9,7 +9,7 @@ layout: none
     // Register the service worker
     navigator.serviceWorker
       .register("pwabuilder-sw.js", {
-        scope: "{{ site.url }}"
+        scope: "{{ site.url }}/"
       })
       .then(function (reg) {
         console.log("Service worker has been registered for scope: " + reg.scope);
