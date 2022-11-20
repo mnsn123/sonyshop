efff646c2f72e2846d558dbce1fb84d315660d3e
mnsn123 <114860079+mnsn123@users.noreply.github.com>
2022-11-20 23:49:18 +0530

    Update sw-reg.js

--- js/sw-reg.js
+++ js/sw-reg.js
@@ -8,8 +8,8 @@ layout: none
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("{{ site.url }}/pwabuilder-sw.js", {
-        scope: "/sonyshop"
+      .register("/pwabuilder-sw.js", {
+        scope: "{{ site.url }}/"
       })
       .then(function (reg) {
         console.log("Service worker has been registered for scope: " + reg.scope);
