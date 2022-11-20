d3c1ba0f95249488d1a5fa9afe190b363b840a20
mnsn123 <114860079+mnsn123@users.noreply.github.com>
2022-11-21 01:13:56 +0530

    Update sw-reg.js

--- js/sw-reg.js
+++ js/sw-reg.js
@@ -13,7 +13,7 @@ layout: none
       })
       .then(function (reg) {
         console.log("Service worker has been registered for scope: " + reg.scope);
-          messaging.useServiceWorker(reg);
+          firebase.messaging().useServiceWorker(reg);
       });
   }
 }
