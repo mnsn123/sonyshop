1d19b50bca943278a3a5f972ad3299f992a4a33f
mnsn123 <114860079+mnsn123@users.noreply.github.com>
2025-06-19 16:01:14 +0530

    Update infinite.js

--- js/infinite.js
+++ js/infinite.js
@@ -1,94 +1,89 @@
 ---
 ---
 
-$(function() {
-  
-  var postURLs,
+$(function () {
+  var postURLs = [],
       isFetchingPosts = false,
       shouldFetchPosts = true,
       postsToLoad = $(".post-list").children().length,
       loadNewPostsThreshold = 3000;
-  
-  // Load the JSON file containing all URLs
-  $.getJSON('{{site.url}}/all-posts.json', function(data) {
+
+  // Disable fetching if no spinner is found
+  if ($(".infinite-spinner").length < 1) shouldFetchPosts = false;
+
+  // Load post URLs from JSON
+  $.getJSON("{{ site.url }}/all-posts.json", function (data) {
     postURLs = data["posts"];
-    
-    // If there aren't any more posts available to load than already visible, disable fetching
-    if (postURLs.length <= postsToLoad)
+
+    // No more posts to fetch?
+    if (postURLs.length <= postsToLoad) {
       disableFetching();
+    }
   });
-	
-  // If there's no spinner, it's not a page where posts should be fetched
-  if ($(".infinite-spinner").length < 1)
-    shouldFetchPosts = false;
-	
-  // Are we close to the end of the page? If we are, load more posts
-  $(window).scroll(function(e){
+
+  // Scroll listener
+  $(window).on("scroll", function () {
     if (!shouldFetchPosts || isFetchingPosts) return;
-    
-    var windowHeight = $(window).height(),
-        windowScrollPosition = $(window).scrollTop(),
-        bottomScrollPosition = windowHeight + windowScrollPosition,
-        documentHeight = $(document).height();
-    
-    // If we've scrolled past the loadNewPostsThreshold, fetch posts
-    if ((documentHeight - loadNewPostsThreshold) < bottomScrollPosition) {
+
+    var scrollBottom = $(window).scrollTop() + $(window).height();
+    var docHeight = $(document).height();
+
+    if (scrollBottom > (docHeight - loadNewPostsThreshold)) {
       fetchPosts();
     }
   });
-  
-  // Fetch a chunk of posts
+
   function fetchPosts() {
-    // Exit if postURLs haven't been loaded
-    if (!postURLs) return;
-    
+    if (!postURLs.length) return;
+
     isFetchingPosts = true;
-    
-    // Load as many posts as there were present on the page when it loaded
-    // After successfully loading a post, load the next one
-    var loadedPosts = 0,
-        postCount = $(".post-list").children().length,
-        callback = function() {
-          loadedPosts++;
-          var postIndex = postCount + loadedPosts;
-          
-          if (postIndex > postURLs.length-1) {
-            disableFetching();
-            return;
-          }
-          
-          if (loadedPosts < postsToLoad) {
-            fetchPostWithIndex(postIndex, callback);
-          } else {
-            isFetchingPosts = false;
-          }
-        };
-		
-    fetchPostWithIndex(postCount + loadedPosts, callback);
+
+    var loadedPosts = 0;
+    var currentCount = $(".post-list").children().length;
+
+    const callback = function () {
+      loadedPosts++;
+      var nextIndex = currentCount + loadedPosts;
+
+      if (nextIndex >= postURLs.length) {
+        disableFetching();
+        return;
+      }
+
+      if (loadedPosts < postsToLoad) {
+        fetchPostWithIndex(nextIndex, callback);
+      } else {
+        isFetchingPosts = false;
+      }
+    };
+
+    fetchPostWithIndex(currentCount, callback);
   }
-	
+
   function fetchPostWithIndex(index, callback) {
     var postURL = postURLs[index];
-		
-    $.get(postURL, function(data) {
-      $(data).find(".post").appendTo(".post-list");
+
+    if (!postURL) {
+      disableFetching();
+      return;
+    }
+
+    $.get(postURL, function (data) {
+      var $post = $(data).find(".post").first();
+
+      if ($post.length) {
+        $(".post-list").append($post);
+      }
+      callback();
+    }).fail(function () {
+      console.warn("Failed to load:", postURL);
       callback();
     });
   }
-  
+
   function disableFetching() {
     shouldFetchPosts = false;
     isFetchingPosts = false;
     $(".infinite-spinner").fadeOut();
   }
-	
 });
-
-
-// var p = document.querySelector('.email_checkout');
-// // console.log(email_content);
-// var a = document.createElement('a');
-// a.className = 'btn btn-primary btn-md rounded-pill email_checkout2';
-// // a.href = '#';
-// a.innerHTML = 'Book through Email';
-// p.append(a);
